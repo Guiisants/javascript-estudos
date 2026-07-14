d09a869db68d07b9b43335b77013e39e963e1a5f
Guilherme Santos <gssilva15@gmail.com>
2026-07-13 22:23:13 -0300

    Aprendendo Idênticos

--- 2_OPERADORES/script.js
+++ 2_OPERADORES/script.js
@@ -61,3 +61,10 @@ console.log(5 <= 5);
 console.log( 5 < 5);
 console.log(5 != 5);
 console.log(5 === 5);
+
+// Idênticos
+console.log(9 == "9");
+console.log(9 + "9");
+console.log(9 === 9);
+console.log(9 != "9");
+console.log(9 !=="9");
